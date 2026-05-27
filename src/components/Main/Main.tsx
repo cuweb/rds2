91237113eba0eb84cb39ff20b9138b8e9e0c7d66
troychaplin <troy.chaplin@gmail.com>
2026-05-27 16:01:46 -0400

    add class name to main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,13 +5,14 @@ export interface MainProps {
   children: React.ReactNode;
   as?: 'main' | 'div';
   hasPadding?: boolean;
+  className?: string;
 }
 
-export const Main = ({ children, as = 'main', hasPadding = true }: MainProps) => {
+export const Main = ({ children, as = 'main', hasPadding = true, className = '' }: MainProps) => {
     const MainWrapper = as;
 
     return (
-        <MainWrapper className={`${hasPadding ? 'cu-main cu-main--padding' : 'cu-main'}`}>
+        <MainWrapper className={`${hasPadding ? 'cu-main cu-main--padding' : 'cu-main'} ${className}`}>
             <div className="alignfull has-global-padding is-layout-constrained entry-content">{children}</div>
         </MainWrapper>
     );
